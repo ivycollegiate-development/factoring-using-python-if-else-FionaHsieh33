cf054391390fbb9892161968a825a3d71ae67d54
FionaHsieh33 <fhsieh26_student@ivycollegiate.org>
2024-10-07 08:06:50 +0000

    Adds logic to check factors

--- fiona_Sprint_Projectv2.75/fiona_Sprint_Projectv2.75.py
+++ fiona_Sprint_Projectv2.75/fiona_Sprint_Projectv2.75.py
@@ -2,4 +2,23 @@
 def list_factors(n):
     # Explain the line of code below
     return [i for i in range (1, n+1) if n % i == 0]
-    # Comment your explination here
+    # This will take whatever number we put in and check for factors using a list
+
+if _name_ == '_main_': #Checks to see if the applicationa is running locally
+    # Ask the user for a number
+    user_input = input('Please enter a number here: ')
+
+try:
+    # Convert that user input to an interger
+    number = int(user_input)
+    factors = list_factors(number)
+    # Print the factors of the input value
+    Print(f'The factors of {number} are {factors}')
+else:
+    # Get the factors for that value input
+    factors = list_factors(number)
+    # Print the factors of the input value
+    Print(f'The factors of {number} are {factors}')
+except valueError:
+    #Handles the exception negative values or 0
+    print('That is an invalid number. Please input a posiyive interger.')
